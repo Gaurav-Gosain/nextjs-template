a300d6b220ff754e5765c9ab9aa51ff5a3aa2298
Gaurav Gosain <gg68@hw.ac.uk>
2022-07-22 02:44:52 +0400

    replaced firebaseui with custom basic button

--- pages/login.js
+++ pages/login.js
@@ -1,35 +1,37 @@
-import { CircularProgress } from "@mui/material";
-import { useAuthState } from "react-firebase-hooks/auth";
+import { Button, CircularProgress } from "@mui/material";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
 import { auth } from "../components/Auth/firebaseAuth";
-import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
-import { GoogleAuthProvider } from "firebase/auth";
-
-// Configure FirebaseUI.
-const uiConfig = {
-  // Popup signin flow rather than redirect flow.
-  signInFlow: "popup",
-  // We will display GitHub as auth providers.
-  signInOptions: [GoogleAuthProvider.PROVIDER_ID],
-};
+import GoogleIcon from "@mui/icons-material/Google";
 
 export default function Login() {
-  const [user, loading, error] = useAuthState(auth);
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
 
   const LoginButton = () => {
     if (loading) return <CircularProgress className="text-6xl" />;
     if (error) return <div>{error.message}</div>;
-    if (user === null)
-      return <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={auth} />;
+    if (user === undefined)
+      return (
+        <Button
+          variant="contained"
+          color="primary"
+          className="text-lg md:text-2xl font-bold p-4 rounded-3xl"
+          startIcon={<GoogleIcon className="text-4xl mr-3" />}
+          onClick={() => {
+            signInWithGoogle();
+          }}>
+          Sign In With Google
+        </Button>
+      );
   };
 
   return (
     <section className="h-screen">
       <div className="h-full px-6 text-gray-800">
         <div className="flex flex-wrap items-center justify-center h-full xl:justify-center lg:justify-between g-6">
-          <div className="mb-12 grow-0 shrink-1 md:shrink-0 basis-auto xl:w-6/12 lg:w-6/12 md:w-9/12 md:mb-0">
+          <div className="grow-0 shrink-1 md:shrink-0 basis-auto xl:w-6/12 lg:w-6/12 md:w-9/12 md:mb-0">
             <img src="/login.svg" className="w-full" alt="Login Image" />
           </div>
-          <div className="flex flex-col-reverse gap-4 mb-12 md:flex-col xl:ml-20 xl:w-5/12 lg:w-5/12 md:w-8/12">
+          <div className="items-center flex flex-col-reverse gap-14 mb-12 md:flex-col xl:ml-20 xl:w-5/12 lg:w-5/12 md:w-8/12">
             <img src="/GDSC_logo.png" className="w-full" alt="Login Image" />
             <LoginButton />
           </div>
